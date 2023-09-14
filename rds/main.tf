8be46c0f93da5268dbac191c036da4e4c7ac0241
Lauren Johnson <lchristopher.johnson@gmail.com>
2023-09-14 01:34:35 -0400

    created rds module

---
+++ rds/main.tf
@@ -0,0 +1,55 @@
+# create database subnet group
+resource "aws_db_subnet_group" "database_subnet_group" {
+  name        = "${var.project_name}-${var.environment}-database-subnets"
+  subnet_ids  = [var.private_data_subnet_az1_id, var.private_data_subnet_az2_id]
+  description = "subnets for database instance"
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-database-subnets"
+  }
+}
+
+# get information about a database snapshot
+#data "aws_db_snapshot" "latest_db_snapshot" {
+ # db_snapshot_identifier = 
+  #most_recent            = 
+  #snapshot_type          = 
+#}
+
+# launch an rds instance from a database snapshot
+# instance_class         = 
+ # skip_final_snapshot    = 
+  #availability_zone      = 
+  #identifier             = 
+  #snapshot_identifier    = 
+  #db_subnet_group_name   = 
+  #multi_az               = 
+  #vpc_security_group_ids = 
+#}
+
+resource "aws_db_instance" "db_instance" {
+  engine                 = "mysql"
+  engine_version         = "8.0.31"
+  multi_az               = false
+  identifier             = "dev-rds-instance"
+  instance_class         = db.t2.micro
+  allocated_storage      = 200
+  db_subnet_group_name   = aws_db_subnet_group.database_subnet_group.name
+  vpc_security_group_ids = [var.database_security_group_id]
+  availability_zone      = data.aws_availability_zones.available_zones.names[0]
+  db_name                = "applicationdb"
+  username               = "laurenj"
+  password               = "test0042"
+  skip_final_snapshot    = true
+
+/*resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = var.dbengine
+  engine_version       = var.e_version
+  instance_class       = var.i_class
+  db_name              = var.dbname
+  username             = var.usrname
+  password             = var.pword
+  parameter_group_name = var.pg_name
+  skip_final_snapshot  = var.skipfs */
+}
---
+++ rds/variables.tf
@@ -0,0 +1,6 @@
+# rds variables
+variable "project_name" {}
+variable "environment" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "database_security_group_id" {}
